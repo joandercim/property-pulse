daeb34e01493222e47d05dcc2c1f361210e24db0
joandercim <joacim@josiasmusik.se>
2024-03-21 22:12:19 +0100

    added mongo connection

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,20 +1,41 @@
 import PropertyCard from '@/components/PropertyCard';
-import properties from '@/properties.json';
+import { revalidatePath } from 'next/cache';
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    revalidatePath(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`)
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
+      )
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties()
+
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
-        { properties.length === 0 ? (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
           <p>No properties found.</p>
         ) : (
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        { properties.map(property => (
-          <PropertyCard key={properties._id} property={property} />
-        ))  }
-      </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={properties._id} property={property} />
+            ))}
+          </div>
         )}
-        </div>
+      </div>
     </section>
   );
 };
