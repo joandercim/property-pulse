70a86ff2f80cf7991fb797101b941525ebee56bc
Joacim Andersson <joacim@josiasmusik.se>
2024-03-21 08:58:18 +0100

    created layout, homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+    title: 'PropertyPulse | Find the perfect rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rental, find properties'
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+export default MainLayout;
